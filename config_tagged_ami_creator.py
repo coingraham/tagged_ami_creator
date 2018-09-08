5f674f2993f605578b0b7145e83487dc12741417
Coin Graham <coingraham@users.noreply.github.com>
2018-09-08 15:24:01 -0400

    Uploaded Files

---
+++ config_tagged_ami_creator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# AWS profile you are referencing for authentication.  If you don't have a profile set to "default"
+# aws_profile = "default"
+aws_profile = "firstwatch"
+
+# AWS region you want to run in.
+aws_region = "us-east-1"  # for local testing
+# aws_region = "us-east-1"
+
+# Comma delimited array of quoted instance ids to create AMIs for.
+instance_ids = ["i-076e8f0c1272e75ba"]
---
+++ tagged_ami_creator.py
@@ -0,0 +1,74 @@
+import boto3
+import botocore.exceptions as boto_fail
+import config_tagged_ami_creator
+import time
+
+
+class TaggedAMICreator:
+    def __init__(self):
+
+        # Set up AWS Session + Client + Resources + Waiters
+        self.aws_profile = config_tagged_ami_creator.aws_profile
+        self.aws_region = config_tagged_ami_creator.aws_region
+
+        # Create custom session
+        self.session = boto3.session.Session(profile_name=self.aws_profile, region_name=self.aws_region)
+
+        # Pre-create the clients for reuse
+        self.ec2_client = self.session.client("ec2")
+        self.ec2_resource = self.session.resource("ec2")
+
+        self.waiter_image_available = self.ec2_client.get_waiter("image_available")
+
+    def take_image(self, instance_ids):
+
+        report = ""
+
+        for instance_id in instance_ids:
+
+            timestr = time.strftime("%m%d%Y-%H%M")
+
+            instance = self.ec2_resource.Instance(instance_id)
+
+            instance_tags = instance.tags
+            remove_tags = instance_tags
+            instance_name = ""
+
+            for bad_tag in remove_tags:
+                if str(bad_tag["Key"]).startswith("aws:"):
+                    instance_tags.remove(bad_tag)
+
+            for tag in instance_tags:
+                if tag["Key"] == "Name":
+                    instance_name = tag["Value"]
+                    tag["Value"] = "{}_{}".format(instance_name, timestr)
+
+            # print(instance_tags)
+
+            image = instance.create_image(
+                Description="AMI Image for {}".format(instance_name),
+                Name="{}_{}".format(instance_name, timestr),
+                NoReboot=True
+            )
+
+            report += "Created image {} for instance {} named {}.\n".format(image.id, instance.id, instance_name)
+
+            try:
+                self.waiter_image_available.wait(
+                    ImageIds=[
+                        image.id,
+                    ]
+                )
+            except boto_fail.WaiterError as e:
+                raise Exception("ERROR: {} waiting for image id {}".format(e, image.id))
+
+            image.create_tags(Tags=instance_tags)
+
+        return report
+
+
+if __name__ == '__main__':
+
+    my_tagged_ami_creator = TaggedAMICreator()
+
+    print(my_tagged_ami_creator.take_image(config_tagged_ami_creator.instance_ids))
